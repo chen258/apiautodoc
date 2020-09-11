830dff06f2d1c15cd919729734b797853a7865a2
MelodyOf流沙 <370479912@qq.com>
2020-09-11 10:07:26 +0800

    add checkappid method

--- src/Tool.php
+++ src/Tool.php
@@ -12,17 +12,6 @@ class Tool
     public function checkAppId($appid,$appsecret)
     {
         $url = Config::$check_api_url;
-//        $curl = curl_init();
-//        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-//        //设置抓取的url
-//        curl_setopt($curl, CURLOPT_URL,  $url . "?appid=$appid&appsecret=$appsecret");
-//        //设置获取的信息以文件流的形式返回，而不是直接输出。
-//        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-//        //执行命令
-//        $data_curl = curl_exec($curl);
-//        //关闭URL请求
-//        curl_close($curl);
-
         $curlobj = curl_init(); // 初始化
         curl_setopt($curlobj, CURLOPT_URL, $url); // 路径
         curl_setopt($curlobj, CURLOPT_HEADER, 0); // 不显示header，因为不是直接打印所以不用显示header,参数中的1代表true，0代表false
